4ec7a58c9b09aa66e1b69acf5112236e7a3ae582
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-19 12:51:40 +0300

    added the PipeCutter file

---
+++ toolbox/PipeCutter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toolbox
+{
+    internal class PipeCutter : Tool
+    {
+        private string _name;
+        private int _weight;
+        public PipeCutter(string name, int weight) : base(name, weight)
+        {
+            _name = name;
+            _weight = weight;
+        }
+
+        public override void Describe()
+        {
+            Console.WriteLine("Your tool is the PipeCutter.");
+        }
+        public override void Use()
+        {
+            Console.WriteLine("PipeCutter is being used");
+        }
+    }
+}

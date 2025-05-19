b54761873bd04a7147ad4e744b66e05ad4e3336a
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-19 12:47:11 +0300

    added the 4 tool. hammer, wrench, drill, sam.

---
+++ toolbox/Saw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toolbox
+{
+    internal class Saw : Tool
+    {
+        private string _name;
+        private int _weight;
+        public Saw(string name, int weight) : base(name, weight)
+        {
+            _name = name;
+            _weight = weight;
+        }
+
+        public override void Describe()
+        {
+            Console.WriteLine("Your tool is the Saw.");
+        }
+        public override void Use()
+        {
+            Console.WriteLine("Saw is being used");
+        }
+    }
+}
